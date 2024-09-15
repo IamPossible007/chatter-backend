449e51a71b74b4c22827e1f6044f9842e3a41bd0
IamPossible007 <113297734+IamPossible007@users.noreply.github.com>
2024-09-15 15:34:15 +0530

    cors3

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,19 @@ import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  const corsOptions: CorsOptions = {
+    origin: 'https://main.d3g1u3lo2fkskh.amplifyapp.com', // replace this with your origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // if you need to send cookies or authentication headers
+  };
   app.useGlobalPipes(new ValidationPipe());
   app.useLogger(app.get(Logger));
   app.use(cookieParser());
-  app.enableCors();
+  app.enableCors(corsOptions);
   const configService = app.get(ConfigService);
   await app.listen(configService.getOrThrow('PORT'));
 }
